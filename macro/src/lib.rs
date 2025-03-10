dcf0b37dd8f547c08bce22a1215b2a476de6a49e
remy.baranx@gmail.com <remy.baranx@gmail.com>
2025-03-10 17:50:06 +0000

    cleaning

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -1,13 +1,3 @@
-//extern crate cainome;
-//extern crate cairo_lang_defs;
-//extern crate cairo_lang_macro;
-//extern crate cairo_lang_parser;
-//extern crate cairo_lang_primitive_token;
-//extern crate cairo_lang_syntax;
-//extern crate cairo_lang_utils;
-//extern crate smol_str;
-//extern crate starknet_crypto;
-
 mod attributes;
 mod constants;
 mod derives;
